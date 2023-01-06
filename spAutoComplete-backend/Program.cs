503e41ff70cbd7e80f2a50d08ade70782067c3f3
idubim <idubi.m@gmail.com>
2023-01-06 17:01:17 +0200

    summerize cors

--- spAutoComplete-backend/Program.cs
+++ spAutoComplete-backend/Program.cs
@@ -1,10 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
-//Access to XMLHttpRequest at
-//'https://localhost:7152/api/AutoComplete/get_match?fileName=world-cities&query=fa'
-//from origin 'http://localhost:4200' has been blocked by CORS policy: Request header field access-control-allow-credentials
-//is not allowed by Access-Control-Allow-Headers in preflight response.
-//autoComplete.service.ts:9 ERROR Error: Uncaught (in promise): AxiosError: Network Error
-//Add services to the container.
+
 
 
 
@@ -12,10 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", policy => policy.WithOrigins("http://localhost:4200").AllowCredentials());
-});
 
 builder.Services.AddControllersWithViews();
 
@@ -34,14 +25,12 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-app.UseCors("AllowOrigin");
-app.UseCors("AllowCredentials");
+
 
 app.UseCors(builder =>
 {
-    builder.AllowAnyHeader();
-    builder.AllowAnyMethod();
-    builder.WithOrigins("AllowAll");
+    //if I remove this call - the cors will fail  ?? 
+   
 });
 
 
