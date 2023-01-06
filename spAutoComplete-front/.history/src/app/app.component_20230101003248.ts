94fb598f2d2ebcc2e9f70e596e5a27a3cd416c4e
idubim <idubi.m@gmail.com>
2023-01-06 16:29:26 +0200

    fix redundant file

--- spAutoComplete-front/.history/src/app/app.component_20230101003248.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-import { AutoCompleteService } from "./services/autoComplete.service";
-
-export class AppComponent {
-  
-  async searchForAutoComplete(type: string, event: any) {
-    return .console.log(typeof AutoCompleteService);
-  }
-}
--- spAutoComplete-front/.history/src/app/app.component_20230101003303.ts
+++
@@ -1,9 +0,0 @@
-
-import { AutoCompleteService } from "./services/autoComplete.service";
-
-export class AppComponent {
-  
-  async searchForAutoComplete(type: string, event: any) {
-    return AutoCompleteService.
-  }
-}
